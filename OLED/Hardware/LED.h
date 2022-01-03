da9a6291290c1013da1870b957dee43cee828f29
guisongchen <guisongchen@163.com>
2022-01-03 12:40:30 +0800

    OLED samples copy from JKDZHX

---
+++ OLED/Hardware/LED.h
@@ -0,0 +1,12 @@
+#ifndef __LED_H
+#define __LED_H
+
+void LED_init();
+void LED1_on();
+void LED1_off();
+void LED1_revert();
+void LED2_on();
+void LED2_off();
+void LED2_revert();
+
+#endif // __LED_H
---
+++ OLED/User/main.c
@@ -0,0 +1,10 @@
+#include "stm32f10x.h"                  // Device header
+#include "Delay.h"
+#include "OLED.h"
+
+int main(void) {
+    OLED_Init();
+    OLED_ShowString(1, 1, "Hello OLED!");
+    while(1) {
+    }
+}
